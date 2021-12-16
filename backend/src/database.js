3ef5c782a64d3f0791a2134feb6a4987ef660044
didachos24 <didachos24@gmail.com>
2021-12-15 23:02:46 -0600

    All advances screen created with style

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const connect = () => mongoose.connect(process.env.DB, {
     autoIndex: true,
 })
--- src/screens/avances/AvancesScreen.js
+++ src/screens/avances/AvancesScreen.js
@@ -1,32 +1,67 @@
 import * as React from 'react';
-import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+import { grey } from '@mui/material/colors'
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { useQuery, gql } from "@apollo/client";
+import styles from "./Styles.modules.css";
+
+const AVANCES = gql`
+  query AllAdvances{
+    allAdvances {
+      _id
+      project {
+        _id
+        name
+      }
+      user {
+        _id
+        fullName
+      }
+      advance
+      date
+      comments
+    }
+  }
+`;
 
 const AvancesScreen = () => {
+
+  const { data } = useQuery(AVANCES);
+  console.log(data);
 
 
   return(
     <>
-      <Container maxWidth="xs">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <h3>Avances</h3>
-          </Grid>
-        </Grid>
+      <Box xs={{ backgroundColor: 'grey'}}>
+        <Typography component="div">
+          <Box sx={{ textAlign: 'center', m: 1, fontWeight: 'bold' }}>
+            <h1>Avances</h1>
+          </Box>
+        </Typography>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <h4>Autor: ###########</h4>
-          </Grid>
-          <Grid item spacing={6}>
-            <h4>Fecha: ###########</h4>
-          </Grid>
-          <Grid item spacing={12}>
-            <p>Avance##########</p>
-          </Grid>
-        </Grid>
-      </Container>
+          { data.allAdvances.map((avance) => (
+            <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <h2>Proyecto: {avance.project.name}</h2>
+                <h3>Autor: {avance.user.fullName}</h3>
+                <h3>Fecha: {avance.date}</h3>
+                <h3>Avance:</h3>
+                <p>{avance.advance}</p>
+                <h3>Comentarios:</h3>
+                <p>{avance.comments}</p>
+              </CardContent>
+          </Card>
+
+            </Grid>
+          ))}
+        </Grid>        
+      </Box>
     </>
   )
-}
+};
 
 export default AvancesScreen;
