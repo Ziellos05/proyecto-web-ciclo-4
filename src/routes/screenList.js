6c08cc9e9c18a2e49e7b963c8f64d4f60266cc38
didachos24 <didachos24@gmail.com>
2021-12-17 16:05:06 -0600

    Minor changes

--- src/routes/screenList.js
+++ src/routes/screenList.js
@@ -3,19 +3,11 @@ import RegisterScreen from "../screens/users/register/Register";
 import HomeScreen from "../screens/home/Home";
 
 import AvancesScreen from "../screens/avances/AvancesScreen";
-<<<<<<< HEAD
 import EditAvance from "../screens/avances/EditAvance";
-=======
-
->>>>>>> f3e7ae1e6995155228ecbeefed16cd0d9ee795a3
 import ProyectosScreen from '../screens/proyectos/ProyectosScreen';
-import GestionProyectos from "../screens/proyectos/gestion/GestionProyectos";
-import RegistroProyectos from "../screens/proyectos/registro/RegistroProyectos";
-import EditarProyectos from "../screens/proyectos/gestion/EditarProyectos";
-
 import InscripcionesScreen from '../screens/inscripciones/InscripcionesScreen';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
 export const publicListScreen = [
@@ -51,27 +43,6 @@ export const privateListScreen = [
         icon: <ArrowForwardIosIcon />,
     },
     {
-        id: 'RegistroProyectos',
-        path: '/proyectos/registro',
-        component: <RegistroProyectos />,
-        name: 'Registro Proyectos',
-        icon: <ArrowForwardIosIcon />,
-    },
-    {
-        id: 'GestionProyectos',
-        path: '/proyectos/gestion',
-        component: <GestionProyectos />,
-        name: 'Gestión Proyectos',
-        icon: <ArrowForwardIosIcon />,
-    },
-    {
-        id: 'EditarProyectos',
-        path: '/proyectos/gestion/editar/:_id',
-        component: <EditarProyectos />,
-        name: 'Editar Proyectos',
-        icon: <ArrowForwardIosIcon />,
-    },
-    {
         id: 'Inscripciones',
         path: '/inscripciones',
         component: <InscripcionesScreen />,
@@ -85,9 +56,9 @@ export const privateListScreen = [
         name: 'Avances',
         icon: <ArrowForwardIosIcon />,
     },
-    {
+        {
         id: 'EditAvance',
         path: '/editaravance/:id',
         component: <EditAvance />,
-    }
+    },
 ];
